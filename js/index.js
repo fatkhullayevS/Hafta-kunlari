3bb19c1c75ef74a64907f3390c83443e819fe8d5
fatkhullayevS <zfathullayev@gmail.com>
2022-05-26 16:45:22 +0500

    init: the end0

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const elInput = document.querySelector(".form-input");
+const elBtn = document.querySelector(".form-btn");
+const elResult = document.querySelector(".form-result");
+
+elBtn.addEventListener('click', function (e){
+    e.preventDefault()
+
+    const inputValue = Number(elInput.value.trim());
+    console.log(inputValue);
+
+    switch (inputValue) {
+        case 1:
+        elResult.textContent = "Monday"
+        break;
+        case 2:
+            elResult.textContent = "Tuesday"
+            break;
+        case 3:
+            elResult.textContent = "Wednesday"
+            break;
+        case 4:
+            elResult.textContent = "Thursday"
+            break;
+        case 5:
+            elResult.textContent = "Friday"
+            break;
+        case 6:
+            elResult.textContent = "Saturday"
+            break;
+        case 7:
+            elResult.textContent = "Sunday"
+            break;
+        default:
+            elResult.textContent = "Bunday kun yo'q"
+        }
+
+    })
